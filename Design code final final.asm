e93ef9dc2252384e02dc6a37eef37ad07e51259c
Advait Koparkar <advaitkoparkar@gmail.com>
2018-08-07 18:28:01 +0530

    Add files via upload

---
+++ Design code final final.asm
@@ -0,0 +1,414 @@
+#make_bin#
+
+#LOAD_SEGMENT=FFFFh#
+#LOAD_OFFSET=0000h#
+
+#CS=0000h#
+#IP=0000h#
+
+#DS=0000h#
+#ES=0000h#
+
+#SS=0000h#
+#SP=FFFEh#
+
+#AX=0000h#
+#BX=0000h#
+#CX=0000h#
+#DX=0000h#
+#SI=0000h#
+#DI=0000h#
+#BP=0000h#
+
+; add your code here
+         jmp     st1 
+         db     509 dup(0)
+
+;IVT entry for 80H
+         
+          dw     t_isr1
+          dw     0000
+
+;IVT entry for 81H
+
+          dw     t_isr2
+          dw     0000
+
+;data
+          temp dw 0
+          pres dw 0
+          humi dw 0
+          cnt1 db 0
+          temp1 db 0
+          temp2 db 0
+          pres1 db 0
+          pres2 db 0
+          humi1 db 0
+          humi2 db 0
+          nega db 10000b         
+          db     490 dup(0)
+          
+;main program
+          
+st1:      cli    
+
+; intialize ds, es,ss to start of RAM
+          mov       ax,0200h
+          mov       ds,ax
+          mov       es,ax
+          mov       ss,ax
+          mov       sp,0FFFEH
+
+;initialise 8255a
+          mov al,10000000b     
+          out 06H , al
+
+;initialise 8255b
+          mov al,10010010b     
+          out 0Eh , al
+
+;initialise 8254 and load count
+          mov al,00010110b
+          out 16H , al
+          mov al,5
+          out 10H , al
+
+          mov al,01110100b
+          out 16H , al
+          mov ax,10000d
+          out 12H , al
+          mov al, ah
+          out 12H , al
+
+          mov al,10110100b
+          out 16H ,al  
+          mov ax, 30000d        
+          out 14H ,al
+          mov al, ah
+          out 14H , al
+
+;initialise 8259
+          mov al,00010011b
+          out 18h,al
+          mov al,40h
+          out 20h,al
+          mov al,00000011b
+          out 20h,al
+          mov al,11111100b
+          out 20h,al
+sti
+
+;delay for warmup
+	mov al, 11010100b		;clk1 of 100Hz
+	out 16h, al
+	in al, 12h
+	mov dl, al
+	in al, 12h
+	mov dh, al
+	sub dx, 600d			;6 sec delay
+del:	mov al, 11010100b
+	out 16h, al
+	in al, 12h
+	mov bl, al
+	in al, 12h
+	mov bh, al
+	cmp bx, dx
+	ja del
+
+;read once and update display
+call read
+call updt
+
+;display loop forever
+x1:       mov al,0111b
+          add al,nega
+          out 04h,al
+          mov al,temp1
+          and al,0Fh
+          mov cl,4
+          rol al,cl
+          mov bl,pres1
+          and bl,0F0h
+          mov cl,4
+          rol bl,cl
+          add al,bl
+          out 00h,al
+          mov al,humi1
+          and al,0Fh
+          out 02h,al
+
+          mov al,1011b
+          add al,nega
+          out 04h,al
+          mov al,temp2
+          and al,0F0h
+          mov bl,pres1
+          and bl,0Fh
+          add al,bl
+          out 00h,al
+          mov al,humi2
+          and al,0F0h
+          mov cl,4
+          rol al,cl
+          out 02h,al
+
+          mov al,1101b
+          add al,nega
+          out 04h,al
+          mov al,temp2
+          and al,0Fh
+          mov cl,4
+          rol al,cl
+          mov bl,pres2
+          and bl,0F0h
+          mov cl,4
+          rol bl,cl
+          add al,bl
+          out 00h,al
+          mov al,humi2
+          and al,0Fh
+          out 02h,al
+
+          mov al,1110b
+          add al,nega
+          out 04h,al
+          mov al,pres2
+          and al,0Fh
+          out 00h,al
+
+          jmp       x1
+
+;isr for switch input
+t_isr1:   sti
+          call updt
+
+          iret
+
+;isr every 5 min
+t_isr2:
+          sti
+          call read
+
+          inc byte ptr cnt1
+          mov al,12
+          cmp cnt1,al
+          jnz aa
+          mov al,0
+          mov cnt1,al
+          call updt
+
+aa:       iret
+          
+read proc near
+          mov ah,0
+          ;temperature
+          mov al,00h
+          out 0CH,al
+          mov al,10h
+          out 0CH,al
+          mov al,08h
+          out 0CH,al
+          mov al,00h
+          out 0CH,al
+
+x22:      in al,0AH
+          and al,1
+          cmp al,0
+          jnz x22
+
+x23:      in al,0AH
+          and al,1
+          cmp al,0
+          jz x23
+
+          mov al,00h
+          out 0CH,al
+          mov al,00100000b
+          out 0CH,al
+          mov al,00h
+          out 0CH,al
+
+          in al,08H
+          add temp,ax
+
+          ;pressure
+          mov al,01h
+          out 0CH,al
+          mov al,11h
+          out 0CH,al
+          mov al,09h
+          out 0CH,al
+          mov al,01h
+          out 0CH,al
+
+x24:      in al,0AH
+          and al,1
+          cmp al,0
+          jnz x24
+
+x25:      in al,0AH
+          and al,1
+          cmp al,0
+          jz x25
+
+          mov al,01h
+          out 0CH,al
+          mov al,00100001b
+          out 0CH,al
+          mov al,01h
+          out 0CH,al
+
+          in al,08H
+          add pres,ax
+
+          ;humidity
+          mov al,02h
+          out 0CH,al
+          mov al,12h
+          out 0CH,al
+          mov al,0Ah
+          out 0CH,al
+          mov al,02h
+          out 0CH,al
+
+x26:      in al,0AH
+          and al,1
+          cmp al,0
+          jnz x26
+
+x27:      in al,0AH
+          and al,1
+          cmp al,0
+          jz x27
+
+          mov al,02h
+          out 0CH,al
+          mov al,00100010b
+          out 0CH,al
+          mov al,02h
+          out 0CH,al
+
+          in al,08H
+          add humi,ax
+
+          ret
+read endp
+
+updt proc near
+          ;temperature
+          mov ax,temp
+          mov dl,cnt1
+          div dl
+          cmp al,128
+          jae bb
+          mov bl,0
+          mov nega,bl
+          sub al,127
+          neg al
+          add al,127         
+bb:       mov bl,10000b
+          mov nega,bl
+          mov cl,100
+          mul cl
+          mov cl,0FFh
+          div cl
+          sub al,50
+          mov dl,al
+          mov al,ah           ;remainder -> al, rest -> dl
+          mov ah,0
+          mov cl,10
+          mul cl
+          mov cl,0FFh
+          div cl              
+          mov temp2,al
+          mov al,dl
+          mov ah,0
+          mov cl,10
+          div cl
+          mov cl,4
+          rol ah,cl
+          add temp2,ah
+          mov temp1,al
+
+          ;pressure
+          mov ax,pres
+          mov dl,cnt1
+          div dl
+          mov cl,150
+          mul cl
+          mov cl,0FFh
+          div cl
+          mov cl,2
+          mul cl
+          add ax,800
+
+          mov dx,ax
+          mov cl,100
+          div cl
+          mov ah,0
+          mov cl,10
+          div cl
+          mov cl,4
+          rol al,cl
+          mov pres1,al
+          rol al,cl
+          mov cl,10
+          mul cl
+          mov cl,100
+          mul cl
+          sub dx,ax
+          mov ax,dx
+          mov cl,100
+          div cl
+          add pres1,al
+          mov ah,0
+          mov cl,100
+          mul cl
+          sub dx,ax
+          mov ax,dx
+          mov cl,10
+          div cl
+          mov cl,4
+          rol al,cl
+          mov pres2,al
+          add pres2,ah
+
+          ;humidity
+          mov ax,humi
+          mov dl,cnt1
+          div dl
+          mov cl,100
+          mul cl
+          mov cl,0FFh
+          div cl
+          mov dl,al
+          mov al,ah           ;remainder -> al, rest -> dl
+          mov ah,0
+          mov cl,10
+          mul cl
+          mov cl,0FFh
+          div cl              
+          mov humi2,al
+          mov al,dl
+          mov ah,0
+          mov cl,10
+          div cl
+          mov cl,4
+          rol ah,cl
+          add humi2,ah
+          mov humi1,al
+          mov al, humi2
+          and al, 0Fh
+          mov dl, 5
+          cmp al, dl
+          jl X5
+          mov al, 5
+          jmp X6
+      X5: mov al, 0
+      X6: mov dl, humi2
+          and dl, 0F0h
+          mov humi2, dl
+          add humi2, al
+
+          ret
+updt endp
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Design code final final is the code we wrote for our design
+Proteus code final final is the code for the proteus, taking into account that
+multiple interrupts and some chips are not available in proteus.
